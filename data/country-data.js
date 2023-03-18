6363db55f7151cced48a62db52bb0aa33adfd678
gmcln-rct <producersama@gmail.com>
2023-03-17 20:53:12 -0400

    Clean up data

--- data/country-data.js
+++ data/country-data.js
@@ -1,113 +1,225 @@
-const DUMMY_COUNTRIES = [
- {
-   id: "italy",
-   itemName: "Italy",
-   category: "OW",
-   regions: [  "Piedmont",  "Tuscany",  "Veneto",  "Lombardy",  "Sicily",  "Apulia",  "Friuli-Venezia Giulia",  "Emilia-Romagna",  "Campania",  "Lazio"],
-   link: "https://en.wikipedia.org/wiki/Italian_wine",
-   isFeatured: false
- },
- {
-   id: "australia",
-   itemName: "Australia",
-   category: "NW",
-   regions: [  "Barossa Valley",  "Hunter Valley",  "Margaret River",  "McLaren Vale",  "Yarra Valley",  "Coonawarra",  "Clare Valley",  "Adelaide Hills",  "Swan Valley",  "Langhorne Creek"],
-   link: "https://en.wikipedia.org/wiki/Australian_wine",
-   isFeatured: false
- },
- {
-   id: "spain",
-   itemName: "Spain",
-   category: "OW",
-   regions: [  "Rioja",  "Ribera del Duero",  "Priorat",  "Catalonia",  "La Mancha",  "Rueda",  "Valdepeñas",  "Jerez-Xérès-Sherry",  "Navarra",  "Penedès"],
-   link: "https://en.wikipedia.org/wiki/Spanish_wine",
-   isFeatured: false
- },
- {
-   id: "france",
-   itemName: "France",
-   category: "OW",
-   regions: [  "Bordeaux",  "Burgundy",  "Champagne",  "Loire Valley",  "Alsace",  "Rhone Valley",  "Languedoc-Roussillon",  "Provence",  "Jura",  "Savoie"],
-   link: "https://en.wikipedia.org/wiki/French_wine",
-   isFeatured: false
- },
- {
-   id: "chile",
-   itemName: "Chile",
-   category: "NW",
-   regions: [  "Maipo Valley",  "Casablanca Valley",  "Colchagua Valley",  "Aconcagua Valley",  "Limarí Valley",  "Cachapoal Valley",  "Curicó Valley",  "Elqui Valley",  "San Antonio Valley",  "Itata Valley"],
-   link: "https://en.wikipedia.org/wiki/Chilean_wine",
-   isFeatured: false
- },
- {
-   id: "argentina",
-   itemName: "Argentina",
-   category: "NW",
-   regions: [  "Mendoza",  "Salta",  "San Juan",  "La Rioja",  "Neuquén",  "Rio Negro",  "Catamarca",  "San Luis",  "La Pampa",  "Buenos Aires"],
-   link: "https://en.wikipedia.org/wiki/Argentine_wine",
-   isFeatured: false
- },
- {
-   id: "south-africa",
-   itemName: "South Africa",
-   category: "NW",
-   regions: [  "Stellenbosch",  "Franschhoek",  "Paarl",  "Swartland",  "Walker Bay",  "Robertson",  "Breede River Valley",  "Constantia",  "Tulbagh",  "Olifants River"],
-   link: "https://en.wikipedia.org/wiki/South_African_wine",
-   isFeatured: false
- },
- {
-   id: "germany",
-   itemName: "Germany",
-   category: "OW",
-   regions: [  "Mosel",  "Rheingau",  "Rheinhessen",  "Pfalz",  "Baden",  "Nahe",  "Ahr",  "Mittelrhein",  "Württemberg",  "Hessische Bergstraße"],
-   link: "https://en.wikipedia.org/wiki/German_wine",
-   isFeatured: false
- },
- {
-   id: "united-states",
-   itemName: "United States",
-   category: "NW",
-   regions: [  "Napa Valley",  "Sonoma County",  "Willamette Valley",  "Central Coast",  "Columbia Valley",  "Santa Barbara County",  "Finger Lakes",  "Long Island",  "Paso Robles",  "Russian River Valley"],
-   link: "https://en.wikipedia.org/wiki/American_wine",
-   isFeatured: false
- },
- {
-   id: "portugal",
-   itemName: "Portugal",
-   category: "OW",
-   regions: [  "Douro",  "Alentejo",  "Vinho Verde",  "Bairrada",  "Dão",  "Portuguese Island wines",  "Lisboa",  "Madeira",  "Setúbal Peninsula",  "Trás-os-Montes"],
-   link: "https://en.wikipedia.org/wiki/Portuguese_wine",
-   isFeatured: false
- },
- {
-   id: "china",
-   itemName: "China",
-   category: "NW",
-   regions: ["Ningxia"],
-   link: "https://en.wikipedia.org/wiki/Chinese_wine",
-   isFeatured: false
- }
-]
-    
-    export function getFeaturedCountries() {
-      return DUMMY_COUNTRIES.filter((event) => event.isFeatured);
-    }
-    
-    export function getAllCountries() {
-      return DUMMY_COUNTRIES;
-    }
-    
-    export function getFilteredCountries(dateFilter) {
-      const { year, month } = dateFilter;
-    
-      let filteredCountries = DUMMY_COUNTRIES.filter((event) => {
-        const eventDate = new Date(event.date);
-        return eventDate.getFullYear() === year && eventDate.getMonth() === month - 1;
-      });
-    
-      return filteredCountries;
-    }
-    
-    export function getEventById(id) {
-      return DUMMY_COUNTRIES.find((event) => event.id === id);
-    }
+const COUNTRIES_DATA = [
+  {
+    id: "italy",
+    itemName: "Italy",
+    category: "OW",
+    regions: [
+      "Piedmont",
+      "Tuscany",
+      "Veneto",
+      "Lombardy",
+      "Sicily",
+      "Apulia",
+      "Friuli-Venezia Giulia",
+      "Emilia-Romagna",
+      "Campania",
+      "Lazio",
+    ],
+    link: "https://en.wikipedia.org/wiki/Italian_wine",
+    isFeatured: false,
+  },
+  {
+    id: "australia",
+    itemName: "Australia",
+    category: "NW",
+    regions: [
+      "Barossa Valley",
+      "Hunter Valley",
+      "Margaret River",
+      "McLaren Vale",
+      "Yarra Valley",
+      "Coonawarra",
+      "Clare Valley",
+      "Adelaide Hills",
+      "Swan Valley",
+      "Langhorne Creek",
+    ],
+    link: "https://en.wikipedia.org/wiki/Australian_wine",
+    isFeatured: false,
+  },
+  {
+    id: "spain",
+    itemName: "Spain",
+    category: "OW",
+    regions: [
+      "Rioja",
+      "Ribera del Duero",
+      "Priorat",
+      "Catalonia",
+      "La Mancha",
+      "Rueda",
+      "Valdepeñas",
+      "Jerez-Xérès-Sherry",
+      "Navarra",
+      "Penedès",
+    ],
+    link: "https://en.wikipedia.org/wiki/Spanish_wine",
+    isFeatured: false,
+  },
+  {
+    id: "france",
+    itemName: "France",
+    category: "OW",
+    regions: [
+      "Bordeaux",
+      "Burgundy",
+      "Champagne",
+      "Loire Valley",
+      "Alsace",
+      "Rhone Valley",
+      "Languedoc-Roussillon",
+      "Provence",
+      "Jura",
+      "Savoie",
+    ],
+    link: "https://en.wikipedia.org/wiki/French_wine",
+    isFeatured: false,
+  },
+  {
+    id: "chile",
+    itemName: "Chile",
+    category: "NW",
+    regions: [
+      "Maipo Valley",
+      "Casablanca Valley",
+      "Colchagua Valley",
+      "Aconcagua Valley",
+      "Limarí Valley",
+      "Cachapoal Valley",
+      "Curicó Valley",
+      "Elqui Valley",
+      "San Antonio Valley",
+      "Itata Valley",
+    ],
+    link: "https://en.wikipedia.org/wiki/Chilean_wine",
+    isFeatured: false,
+  },
+  {
+    id: "argentina",
+    itemName: "Argentina",
+    category: "NW",
+    regions: [
+      "Mendoza",
+      "Salta",
+      "San Juan",
+      "La Rioja",
+      "Neuquén",
+      "Rio Negro",
+      "Catamarca",
+      "San Luis",
+      "La Pampa",
+      "Buenos Aires",
+    ],
+    link: "https://en.wikipedia.org/wiki/Argentine_wine",
+    isFeatured: false,
+  },
+  {
+    id: "south-africa",
+    itemName: "South Africa",
+    category: "NW",
+    regions: [
+      "Stellenbosch",
+      "Franschhoek",
+      "Paarl",
+      "Swartland",
+      "Walker Bay",
+      "Robertson",
+      "Breede River Valley",
+      "Constantia",
+      "Tulbagh",
+      "Olifants River",
+    ],
+    link: "https://en.wikipedia.org/wiki/South_African_wine",
+    isFeatured: false,
+  },
+  {
+    id: "germany",
+    itemName: "Germany",
+    category: "OW",
+    regions: [
+      "Mosel",
+      "Rheingau",
+      "Rheinhessen",
+      "Pfalz",
+      "Baden",
+      "Nahe",
+      "Ahr",
+      "Mittelrhein",
+      "Württemberg",
+      "Hessische Bergstraße",
+    ],
+    link: "https://en.wikipedia.org/wiki/German_wine",
+    isFeatured: false,
+  },
+  {
+    id: "united-states",
+    itemName: "United States",
+    category: "NW",
+    regions: [
+      "Napa Valley",
+      "Sonoma County",
+      "Willamette Valley",
+      "Central Coast",
+      "Columbia Valley",
+      "Santa Barbara County",
+      "Finger Lakes",
+      "Long Island",
+      "Paso Robles",
+      "Russian River Valley",
+    ],
+    link: "https://en.wikipedia.org/wiki/American_wine",
+    isFeatured: false,
+  },
+  {
+    id: "portugal",
+    itemName: "Portugal",
+    category: "OW",
+    regions: [
+      "Douro",
+      "Alentejo",
+      "Vinho Verde",
+      "Bairrada",
+      "Dão",
+      "Portuguese Island wines",
+      "Lisboa",
+      "Madeira",
+      "Setúbal Peninsula",
+      "Trás-os-Montes",
+    ],
+    link: "https://en.wikipedia.org/wiki/Portuguese_wine",
+    isFeatured: false,
+  },
+  {
+    id: "china",
+    itemName: "China",
+    category: "NW",
+    regions: ["Ningxia"],
+    link: "https://en.wikipedia.org/wiki/Chinese_wine",
+    isFeatured: false,
+  },
+];
+
+export function getFeaturedCountries() {
+  return COUNTRIES_DATA.filter((event) => event.isFeatured);
+}
+
+export function getAllCountries() {
+  return COUNTRIES_DATA;
+}
+
+export function getFilteredCountries(dateFilter) {
+  const { year, month } = dateFilter;
+
+  let filteredCountries = COUNTRIES_DATA.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
+  return filteredCountries;
+}
+
+export function getCountryById(id) {
+  return COUNTRIES_DATA.find((event) => event.id === id);
+}
--- data/grape-data.js
+++ data/grape-data.js
@@ -1,193 +1,195 @@
-const DUMMY_EVENTS = [
+const GRAPES_DATA = [
   {
-      id: "cabernet-sauvignon",
-      itemName: "Cabernet Sauvignon",
-      category: "R",
-      altNames: "[]",
-      link: "https://en.wikipedia.org/wiki/Cabernet_Sauvignon",
-      isFeaured: "FALSE"
+    id: "cabernet-sauvignon",
+    itemName: "Cabernet Sauvignon",
+    category: "R",
+    altNames: "[]",
+    link: "https://en.wikipedia.org/wiki/Cabernet_Sauvignon",
+    isFeaured: "FALSE",
   },
   {
-      id: "merlot",
-      itemName: "Merlot",
-      category: "R",
-      altNames: "[]",
-      link: "https://en.wikipedia.org/wiki/Merlot",
-      isFeaured: "FALSE"
+    id: "merlot",
+    itemName: "Merlot",
+    category: "R",
+    altNames: "[]",
+    link: "https://en.wikipedia.org/wiki/Merlot",
+    isFeaured: "FALSE",
   },
   {
-      id: "tempranillo",
-      itemName: "Tempranillo",
-      category: "R",
-      altNames: "[]",
-      link: "https://en.wikipedia.org/wiki/Tempranillo",
-      isFeaured: "FALSE"
+    id: "tempranillo",
+    itemName: "Tempranillo",
+    category: "R",
+    altNames: "[]",
+    link: "https://en.wikipedia.org/wiki/Tempranillo",
+    isFeaured: "FALSE",
   },
   {
-      id: "airén",
-      itemName: "Airén",
-      category: "W",
-      altNames: "['Airen']",
-      link: "https://en.wikipedia.org/wiki/Air%C3%A9n",
-      isFeaured: "FALSE"
+    id: "airén",
+    itemName: "Airén",
+    category: "W",
+    altNames: "['Airen']",
+    link: "https://en.wikipedia.org/wiki/Air%C3%A9n",
+    isFeaured: "FALSE",
   },
   {
-      id: "chardonnay",
-      itemName: "Chardonnay",
-      category: "W",
-      altNames: "[]",
-      link: "https://en.wikipedia.org/wiki/Chardonnay",
-      isFeaured: "FALSE"
+    id: "chardonnay",
+    itemName: "Chardonnay",
+    category: "W",
+    altNames: "[]",
+    link: "https://en.wikipedia.org/wiki/Chardonnay",
+    isFeaured: "FALSE",
   },
   {
-      id: "syrah",
-      itemName: "Syrah",
-      category: "R",
-      altNames: "['Shiraz']",
-      link: "https://en.wikipedia.org/wiki/Syrah",
-      isFeaured: "FALSE"
+    id: "syrah",
+    itemName: "Syrah",
+    category: "R",
+    altNames: "['Shiraz']",
+    link: "https://en.wikipedia.org/wiki/Syrah",
+    isFeaured: "FALSE",
   },
   {
-      id: "garnacha-tinta",
-      itemName: "Garnacha Tinta",
-      category: "R",
-      altNames: "['Grenache']",
-      link: "https://en.wikipedia.org/wiki/Grenache",
-      isFeaured: "FALSE"
+    id: "garnacha-tinta",
+    itemName: "Garnacha Tinta",
+    category: "R",
+    altNames: "['Grenache']",
+    link: "https://en.wikipedia.org/wiki/Grenache",
+    isFeaured: "FALSE",
   },
   {
-      id: "sauvignon-blanc",
-      itemName: "Sauvignon Blanc",
-      category: "W",
-      altNames: "[]",
-      link: "https://en.wikipedia.org/wiki/Sauvignon_blanc",
-      isFeaured: "FALSE"
+    id: "sauvignon-blanc",
+    itemName: "Sauvignon Blanc",
+    category: "W",
+    altNames: "[]",
+    link: "https://en.wikipedia.org/wiki/Sauvignon_blanc",
+    isFeaured: "FALSE",
   },
   {
-      id: "trebbiano-toscano",
-      itemName: "Trebbiano Toscano",
-      category: "W",
-      altNames: "[]",
-      link: "https://en.wikipedia.org/wiki/Trebbiano_Toscano",
-      isFeaured: "FALSE"
+    id: "trebbiano-toscano",
+    itemName: "Trebbiano Toscano",
+    category: "W",
+    altNames: "[]",
+    link: "https://en.wikipedia.org/wiki/Trebbiano_Toscano",
+    isFeaured: "FALSE",
   },
   {
-      id: "pinot-noir",
-      itemName: "Pinot Noir",
-      category: "R",
-      altNames: "[]",
-      link: "https://en.wikipedia.org/wiki/Pinot_noir",
-      isFeaured: "FALSE"
+    id: "pinot-noir",
+    itemName: "Pinot Noir",
+    category: "R",
+    altNames: "[]",
+    link: "https://en.wikipedia.org/wiki/Pinot_noir",
+    isFeaured: "FALSE",
   },
   {
-      id: "sangiovese",
-      itemName: "Sangiovese",
-      category: "R",
-      altNames: "[]",
-      link: "https://en.wikipedia.org/wiki/Sangiovese",
-      isFeaured: "FALSE"
+    id: "sangiovese",
+    itemName: "Sangiovese",
+    category: "R",
+    altNames: "[]",
+    link: "https://en.wikipedia.org/wiki/Sangiovese",
+    isFeaured: "FALSE",
   },
   {
-      id: "riesling",
-      itemName: "Riesling",
-      category: "W",
-      altNames: "[]",
-      link: "https://en.wikipedia.org/wiki/Riesling",
-      isFeaured: "FALSE"
+    id: "riesling",
+    itemName: "Riesling",
+    category: "W",
+    altNames: "[]",
+    link: "https://en.wikipedia.org/wiki/Riesling",
+    isFeaured: "FALSE",
   },
   {
-      id: "bobal",
-      itemName: "Bobal",
-      category: "R",
-      altNames: "[]",
-      link: "https://en.wikipedia.org/wiki/Bobal",
-      isFeaured: "FALSE"
+    id: "bobal",
+    itemName: "Bobal",
+    category: "R",
+    altNames: "[]",
+    link: "https://en.wikipedia.org/wiki/Bobal",
+    isFeaured: "FALSE",
   },
   {
-      id: "cabernet-franc",
-      itemName: "Cabernet Franc",
-      category: "R",
-      altNames: "[]",
-      link: "https://en.wikipedia.org/wiki/Cabernet_Franc",
-      isFeaured: "FALSE"
+    id: "cabernet-franc",
+    itemName: "Cabernet Franc",
+    category: "R",
+    altNames: "[]",
+    link: "https://en.wikipedia.org/wiki/Cabernet_Franc",
+    isFeaured: "FALSE",
   },
   {
-      id: "côt",
-      itemName: "Côt",
-      category: "R",
-      altNames: "['Malbec']",
-      link: "https://en.wikipedia.org/wiki/Cot_(grape)",
-      isFeaured: "FALSE"
+    id: "côt",
+    itemName: "Côt",
+    category: "R",
+    altNames: "['Malbec']",
+    link: "https://en.wikipedia.org/wiki/Cot_(grape)",
+    isFeaured: "FALSE",
   },
   {
-      id: "monastrell",
-      itemName: "Monastrell",
-      category: "R",
-      altNames: "['Mourvedre', 'Mourvèdre', 'Monastrell', 'Mourvèdre']",
-      link: "https://en.wikipedia.org/wiki/Monastrell",
-      isFeaured: "FALSE"
+    id: "monastrell",
+    itemName: "Monastrell",
+    category: "R",
+    altNames: "['Mourvedre', 'Mourvèdre', 'Monastrell', 'Mourvèdre']",
+    link: "https://en.wikipedia.org/wiki/Monastrell",
+    isFeaured: "FALSE",
   },
   {
-      id: "rkatsiteli",
-      itemName: "Rkatsiteli",
-      category: "W",
-      altNames: "[]",
-      link: "https://en.wikipedia.org/wiki/Rkatsiteli",
-      isFeaured: "FALSE"
+    id: "rkatsiteli",
+    itemName: "Rkatsiteli",
+    category: "W",
+    altNames: "[]",
+    link: "https://en.wikipedia.org/wiki/Rkatsiteli",
+    isFeaured: "FALSE",
   },
   {
-      id: "pinot-gris",
-      itemName: "Pinot Gris",
-      category: "G",
-      altNames: "['Pinot Grigio']",
-      link: "https://en.wikipedia.org/wiki/Pinot_gris",
-      isFeaured: "FALSE"
+    id: "pinot-gris",
+    itemName: "Pinot Gris",
+    category: "G",
+    altNames: "['Pinot Grigio']",
+    link: "https://en.wikipedia.org/wiki/Pinot_gris",
+    isFeaured: "FALSE",
   },
   {
-      id: "mazuelo",
-      itemName: "Mazuelo",
-      category: "R",
-      altNames: "[]",
-      link: "https://en.wikipedia.org/wiki/Mazuelo",
-      isFeaured: "FALSE"
+    id: "mazuelo",
+    itemName: "Mazuelo",
+    category: "R",
+    altNames: "[]",
+    link: "https://en.wikipedia.org/wiki/Mazuelo",
+    isFeaured: "FALSE",
   },
   {
-      id: "macabeo",
-      itemName: "Macabeo",
-      category: "W",
-      altNames: "[]",
-      link: "https://en.wikipedia.org/wiki/Macabeo",
-      isFeaured: "FALSE"
+    id: "macabeo",
+    itemName: "Macabeo",
+    category: "W",
+    altNames: "[]",
+    link: "https://en.wikipedia.org/wiki/Macabeo",
+    isFeaured: "FALSE",
   },
   {
-      id: "cayetana-blanca",
-      itemName: "Cayetana Blanca",
-      category: "W",
-      altNames: "[]",
-      link: "https://en.wikipedia.org/wiki/Cayetana_Blanca",
-      isFeaured: "FALSE"
-  }
-]
-  
-  export function getFeaturedEvents() {
-    return DUMMY_EVENTS.filter((event) => event.isFeatured);
-  }
-  
-  export function getAllEvents() {
-    return DUMMY_EVENTS;
-  }
-  
-  export function getFilteredEvents(dateFilter) {
-    const { year, month } = dateFilter;
-  
-    let filteredEvents = DUMMY_EVENTS.filter((event) => {
-      const eventDate = new Date(event.date);
-      return eventDate.getFullYear() === year && eventDate.getMonth() === month - 1;
-    });
-  
-    return filteredEvents;
-  }
-  
-  export function getEventById(id) {
-    return DUMMY_EVENTS.find((event) => event.id === id);
-  }
+    id: "cayetana-blanca",
+    itemName: "Cayetana Blanca",
+    category: "W",
+    altNames: "[]",
+    link: "https://en.wikipedia.org/wiki/Cayetana_Blanca",
+    isFeaured: "FALSE",
+  },
+];
+
+export function getFeaturedGrapes() {
+  return GRAPES_DATA.filter((event) => event.isFeatured);
+}
+
+export function getAllGrapes() {
+  return GRAPES_DATA;
+}
+
+export function getFilteredGrapes(dateFilter) {
+  const { year, month } = dateFilter;
+
+  let filteredGrapes = GRAPES_DATA.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
+  return filteredGrapes;
+}
+
+export function getEventById(id) {
+  return GRAPES_DATA.find((event) => event.id === id);
+}
