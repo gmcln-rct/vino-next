c9fbd43640ff5692abe30ec40f46e4cef56a66a8
gmcln-rct <producersama@gmail.com>
2023-03-19 18:17:56 -0400

    Remove next font

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-// import Head from 'next/head'
-// import Image from 'next/image'
+
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
 
@@ -7,15 +6,19 @@ import { getFeaturedCountries } from "@/data/country-data";
 
 import CountryList from "@/components/countries/country-list";
 
+// const inter = Inter({
+//   variable: '--inter-font',
+// })
 // MAIN HOMEPAGE
 
 function HomePage() {
+
 
   const featuredCountries = getFeaturedCountries();
   return (
     <>
       <h1>Homepage</h1>
-        <CountryList items={ featuredCountries }/>
+      <CountryList items={ featuredCountries }/>
     </>
   );
 }
