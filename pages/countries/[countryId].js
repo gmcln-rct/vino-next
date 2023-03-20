4c24f9088812e233257f398d1eeece22ecb39254
gmcln-rct <producersama@gmail.com>
2023-03-20 09:10:41 -0400

    Work on detail page

--- pages/countries/[countryId].js
+++ pages/countries/[countryId].js
@@ -1,10 +1,18 @@
 import { useRouter } from 'next/router';
 
+import { getCountryById } from '@/data/country-data';
+
 function CountryDetailPage() {
   const router = useRouter();
-  const { pageId } = router.query;
+  // const { pageId } = router.query;
 
-  return <div>Detail page for Country {pageId}</div>;
+  const [params] = router.query.countryId.split('-'); // [ 'countries', 'germany'
+
+  const country = getCountryById(params);
+  
+  console.log('pageId', params, country);
+
+  return <h2>Detail page for Country: {country.itemName}</h2>;
 }
 
 export default CountryDetailPage;
