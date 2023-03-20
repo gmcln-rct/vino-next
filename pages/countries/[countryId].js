3b0be182c143025fcec1814dab6ba85a607036c1
gmcln-rct <producersama@gmail.com>
2023-03-20 09:14:46 -0400

    Work on detail page

--- pages/countries/[countryId].js
+++ pages/countries/[countryId].js
@@ -4,15 +4,20 @@ import { getCountryById } from '@/data/country-data';
 
 function CountryDetailPage() {
   const router = useRouter();
+  
+  const [params] = router.query.countryId.split('-'); 
   // const { pageId } = router.query;
-
-  const [params] = router.query.countryId.split('-'); // [ 'countries', 'germany'
 
   const country = getCountryById(params);
   
   console.log('pageId', params, country);
 
-  return <h2>Detail page for Country: {country.itemName}</h2>;
+  return <>
+    <h2>Detail page for Country: {country.itemName}</h2>;
+    <a href={country.link} target="_blank">Info about Wine Region</a>
+  </>
+  
+  
 }
 
 export default CountryDetailPage;
