eb0f95c2e7ccebfd1096dcef84533dc9b8954765
gmcln-rct <producersama@gmail.com>
2023-03-22 11:36:58 -0400

    Work on header

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,33 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import Layout from '../components/layout/layout';
+import Layout from "../components/layout/layout";
 // import HomePage from './index';
-import MainHeader from '../components/layout/main-header';
-import '../styles/globals.css';
+import MainHeader from "../components/layout/main-header";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <MainHeader />
-    <main>
-      <Component {...pageProps} />
-    </main>
+      <MainHeader />`{" "}
+      <Head>
+        <title>Winography - Wine Data Visualization</title>
+        <meta name="description" content="Winography - Data visualization about wine production and consumption, both historic and present-day" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      `
+      <main>
+        <Component {...pageProps} />
+      </main>
     </>
     // <NotificationContextProvider>
-      // <Layout >
-      //   <Head>
-      //     <title>Winography - Next 202303</title>
-      //     <meta name='description' content='NextJS Events' />
-      //     <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      //   </Head>
-      //   <Component {...pageProps} />
-      // </Layout>
+    // <Layout >
+    //   <Head>
+    //     <title>Winography - Next 202303</title>
+    //     <meta name='description' content='NextJS Events' />
+    //     <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    //   </Head>
+    //   <Component {...pageProps} />
+    // </Layout>
     // </NotificationContextProvider>
   );
 }
