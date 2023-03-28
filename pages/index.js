968b2f7051ae31541fbee9c390e19b862e61af35
gmcln-rct <producersama@gmail.com>
2023-03-28 11:01:21 -0400

    Work on home

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,10 @@ function HomePage() {
           content="Winography - Data visualization about wine production and consumption, both historic and present-day"
         />
       </Head>
+      <section className="masthead">
       <h1>Homepage</h1>
+      <p>Winography presents the wold of wine through data about wine production for different countries and grapes around the world. It is a resource intended for wine professionals and enthusiasts alike.</p>
+      </section>
       <h2>Browse data visualizations by type:</h2>
       <section className="home">
         <Link href="/countries" className="home__main center">
