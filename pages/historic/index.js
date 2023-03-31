fce4f61a4a03761f1b789b66bad00b4cbc223b44
gmcln-rct <producersama@gmail.com>
2023-03-31 11:46:19 -0400

    Work on stacked

--- pages/historic/index.js
+++ pages/historic/index.js
@@ -1,13 +1,18 @@
 import Head from "next/head";
-
+import { useState } from "react";
 
 import StreamGraph from "@/components/graphs/streamgraph-chart";
 import TimelineChart from "@/components/graphs/streamgraph-alt";
 import StackedBarChart from "@/components/graphs/stacked-area-chart";
 
+import HISTORIC_PRODUCTION_STACKED_DATA from "@/data/historic-production-stacked-data";
+
+
 function HistoricDataIndex() {
 
-  // console.log('countries data: ', allCountries);
+  const [historicData, setHistoricData] = useState(HISTORIC_PRODUCTION_STACKED_DATA);
+
+  console.log('countries data: ', historicData);
 
 //   if (!COUNTRIES_DATA) {
 //     return <p>Loading...</p>;
@@ -29,7 +34,7 @@ function HistoricDataIndex() {
       <h1>Top 10 Wine Producing Countries</h1>
       {/* <CountriesList items={topCountries} /> */}
       {/* <TimelineChart /> */}
-      <StackedBarChart />
+      <StackedBarChart historicData = {historicData} />
     </>
   );
 }
