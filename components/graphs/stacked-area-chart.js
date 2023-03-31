4dc80d3dadc82d10df75b27b8273a7f0657d3d32
gmcln-rct <producersama@gmail.com>
2023-03-31 12:56:55 -0400

    Update stacked chart

--- components/graphs/stacked-area-chart.js
+++ components/graphs/stacked-area-chart.js
@@ -1,28 +1,28 @@
 import React, { useRef, useEffect } from "react";
 import * as d3 from "d3";
 
-import HISTORIC_PRODUCTION_STACKED_DATA from "@/data/historic-production-stacked-data";
-
+import { HISTORIC_PRODUCTION_STACKED_DATA } from "@/data/historic-production-stacked-data";
 
 const StackedAreaChart = () => {
   const svgRef = useRef();
   const tooltipRef = useRef();
-  
-  const data =  HISTORIC_PRODUCTION_STACKED_DATA;
+
+  const data = HISTORIC_PRODUCTION_STACKED_DATA;
 
   console.log("data from json: ", data);
 
   if (!data) {
     return <div>Loading...</div>;
-  };
+  }
   const objArray = Object.keys(data[0]).slice(1);
 
   console.log("array of objects: ", objArray);
 
   useEffect(() => {
-    const margin = { top: 20, right: 30, bottom: 50, left: 60 };
+    d3.select(svgRef.current).selectAll("*").remove();
+    const margin = { top: 20, right: 30, bottom: 100, left: 60 }; // Updated bottom margin value
     const width = 800;
-    const height = 300;
+    const height = 320;
 
     const svg = d3
       .select(svgRef.current)
@@ -30,6 +30,15 @@ const StackedAreaChart = () => {
       .append("g")
       .attr("transform", `translate(${margin.left},${margin.top})`);
 
+      svg.append("rect")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", width - margin.left - margin.right)
+      .attr("height", height - margin.top - margin.bottom)
+      .style("fill", "#a9a9a9")
+      .style("opacity", 0.8);
+
+    // Create scales
     const xScale = d3
       .scaleLinear()
       .domain([d3.min(data, (d) => d.year), d3.max(data, (d) => d.year)])
@@ -56,33 +65,37 @@ const StackedAreaChart = () => {
       .append("path")
       .attr("fill", (d) => colors(d.key))
       .attr("d", area);
+
     const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
     const yAxis = d3.axisLeft(yScale);
 
     svg
       .append("g")
+      .attr("class", "x-axis")
       .attr("transform", `translate(0,${height - margin.top - margin.bottom})`)
       .call(xAxis);
 
-    svg.append("g").call(yAxis);
+    svg.append("g").attr("class", "y-axis").call(yAxis);
 
     svg
       .append("text")
       .attr(
         "transform",
-        `translate(${width / 2},${height - margin.bottom / 2})`
+        `translate(${width / 2.2},${height - margin.bottom / 2 - 30})`
       )
       .style("text-anchor", "middle")
+      .style("font-size", "0.8em")
       .text("Year");
 
-    svg
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x", 0 - height / 2)
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .text("Production (KL)");
+    // X-axis label
+    // svg
+    //   .append("text")
+    //   .attr("transform", "rotate(-90)")
+    //   .attr("y", 0 - margin.left -10)
+    //   .attr("x", 0 - height / 2)
+    //   .attr("dy", "1em")
+    //   .style("text-anchor", "middle")
+    //   .text("Production (KL)");
 
     // Create a legend
     const legend = svg
@@ -90,8 +103,8 @@ const StackedAreaChart = () => {
       .attr("class", "legend")
       .attr(
         "transform",
-        `translate(${margin.left},${height - margin.bottom + 20})`
-      );
+        `translate(${margin.left},${height - margin.bottom + 30})`
+      ); // Increased padding between chart and legend
 
     const legendItems = legend
       .selectAll(".legend-item")
@@ -99,7 +112,7 @@ const StackedAreaChart = () => {
       .enter()
       .append("g")
       .attr("class", "legend-item")
-      .attr("transform", (d, i) => `translate(${i * 80}, 0)`);
+      .attr("transform", (d, i) => `translate(${i * 50}, 0)`); // Increased spacing between legend items
 
     legendItems
       .append("rect")
@@ -109,9 +122,17 @@ const StackedAreaChart = () => {
 
     legendItems
       .append("text")
-      .attr("x", 25)
-      .attr("y", 15)
-      .text((d) => d);
+      .attr("x", 20)
+      .attr("y", 20)
+      .html((d) => {
+        const words = d.split(" ");
+        return `<tspan x="0" dy="1.4em">${words
+          .slice(0, words.length / 2)
+          .join(" ")}</tspan><tspan x="0" dy="1.4em">${words
+          .slice(words.length / 2)
+          .join(" ")}</tspan>`;
+      })
+      .attr("margin-top", 10);
 
     d3.select(window).on("resize", () => {
       const containerWidth = svgRef.current.getBoundingClientRect().width;
@@ -126,19 +147,13 @@ const StackedAreaChart = () => {
   }, []);
 
   return (
-    <div style={{ position: "relative", width: "90%", height: "100%" }}>
+    <div style={{ position: "relative", width: "90%", height: "90%" }}>
       <svg ref={svgRef}></svg>
-      <div
-        ref={tooltipRef}
-        style={{
-          display: "none",
-          position: "absolute",
-          padding: "10px",
-          background: "white",
-          boxShadow: "0px 0px 5px rgba(0,0,0,0.3)",
-          zIndex: 9999,
-        }}
-      ></div>
+      <div ref={tooltipRef} className="tooltip">
+        Production in KL
+      </div>
+      <br />
+      <p className="center">Wine production in Kiloliters(KL)</p>
     </div>
   );
 };
--- pages/historic/index.js
+++ pages/historic/index.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
+import Link from "next/link";
 import { useState } from "react";
 
-import StreamGraph from "@/components/graphs/streamgraph-chart";
-import TimelineChart from "@/components/graphs/streamgraph-alt";
+import GRAPES_RED_TOP_TEN_DATA from "@/data/grape-top-ten-countries-data-2016";
+
+// import StreamGraph from "@/components/graphs/streamgraph-chart";
+// import TimelineChart from "@/components/graphs/streamgraph-alt";
 import StackedBarChart from "@/components/graphs/stacked-area-chart";
 
 import HISTORIC_PRODUCTION_STACKED_DATA from "@/data/historic-production-stacked-data";
@@ -10,15 +13,15 @@ import HISTORIC_PRODUCTION_STACKED_DATA from "@/data/historic-production-stacked
 
 function HistoricDataIndex() {
 
-  const [historicData, setHistoricData] = useState(HISTORIC_PRODUCTION_STACKED_DATA);
+  // const [historicData, setHistoricData] = useState(HISTORIC_PRODUCTION_STACKED_DATA);
 
-  console.log('countries data: ', historicData);
+  // if (!HISTORIC_PRODUCTION_STACKED_DATA) {
+  //   return <p>Loading...</p>;
+  // }
 
-//   if (!COUNTRIES_DATA) {
-//     return <p>Loading...</p>;
-//   }
+  const data = GRAPES_RED_TOP_TEN_DATA;
 
-//   const topCountries = getTopData(COUNTRIES_DATA);
+  console.log('countries data: ', data);
 
   return (
     <>
@@ -31,10 +34,21 @@ function HistoricDataIndex() {
           content="Data visualization for all wine-producing countries"
         />
       </Head>
-      <h1>Top 10 Wine Producing Countries</h1>
+      <h1>Historic Wine Production - Top Producers</h1>
       {/* <CountriesList items={topCountries} /> */}
       {/* <TimelineChart /> */}
-      <StackedBarChart historicData = {historicData} />
+      <StackedBarChart  />
+      <div>
+        <p className="dataSource">
+          Data Source:{" "}
+          <Link
+            href="https://economics.adelaide.edu.au/wine-economics/databases/"
+            className="dataSource"
+          >
+            Wine Economics Research Centre, University of Adelaide
+          </Link>
+        </p>
+      </div>
     </>
   );
 }
