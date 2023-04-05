5ba607462bb56f08c8bddad8f5326e3127692c1b
gmcln-rct <producersama@gmail.com>
2023-04-05 14:24:27 -0400

    Add icons

--- pages/historic/index.js
+++ pages/historic/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
 
 import Button from "@/components/ui/button";
 
@@ -21,10 +23,30 @@ function HistoricDataIndex() {
       </Head>
       <section className="masthead">
         <h2 className="header"> Historic Production Data</h2>
-        <div className="actions">
+        <Link href="/historic/histogram" className="action__container center">
+            <Image
+              src="/images/icons/icon-histogram.png"
+              className="transparent margin-bottom"
+              alt="grapes icon"
+              width={100}
+              height={100}
+            />
+            <p className="link">Histogram Chart</p>
+          </Link>
+          <Link href="/historic/stackedarea" className="action__container center">
+            <Image
+              src="/images/icons/icon-stackedarea.png"
+              className="transparent margin-bottom"
+              alt="grapes icon"
+              width={100}
+              height={100}
+            />
+            <p className="link">Stacked Area Chart</p>
+          </Link>
+        {/* <div className="actions">
           <Button link={histogramLink}>Histogram Chart</Button>
           <Button link={stackedAreaLink}>Stacked Area Chart</Button>
-        </div>
+        </div> */}
       </section>
     </>
   );
