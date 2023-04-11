55436bf5f1d6464c1766f969887ab45d3062c356
gmcln-rct <producersama@gmail.com>
2023-04-11 12:25:21 -0400

    Update text and compress images

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ function HomePage() {
       </Head>
       <Masthead
         backgroundImage="/images/site-images/homepage-masthead.jpg"
-        headerText="Explore the world of wine through data visualizations by grapes, country or historic yearly trends."
+        headerText="Explore the world of wine through data visualizations."
       />
       <section className="homePage">
         {/* <p className="headline">
