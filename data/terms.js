f5037066602f7180772242882fd3c79f6063aeb0
gmcln-rct <producersama@gmail.com>
2023-04-12 12:46:53 -0400

    Add data

---
+++ data/terms.js
@@ -0,0 +1,210 @@
+export const WINE_TERMS = [
+  {
+    word: "Aeration",
+    definition:
+      "The process of exposing wine to oxygen to enhance its flavors and aromas, often achieved through decanting, swirling, or using a wine aerator.",
+  },
+  {
+    word: "Amphora",
+    definition:
+      "A large, ancient ceramic vessel used for fermenting, storing, and transporting wine, which has experienced a resurgence in popularity for its unique influence on wine flavors and textures.",
+  },
+  {
+    word: "Appellation",
+    definition:
+      "A legally defined geographical area used to identify where grapes for a wine were grown and to maintain quality standards.",
+  },
+  {
+    word: "Balance",
+    definition:
+      "The harmony between a wine's various components, such as acidity, tannins, fruit, and alcohol, resulting in a pleasant and cohesive taste.",
+  },
+  {
+    word: "Blending",
+    definition:
+      "The process of combining different grape varieties or wines to create a final wine with specific characteristics, flavors, and balance.",
+  },
+  {
+    word: "Body",
+    definition:
+      "The weight and fullness of a wine in the mouth, influenced by factors like alcohol content, tannins, and sugar levels.",
+  },
+  {
+    word: "Botrytis",
+    definition:
+      "A type of fungus, also known as 'noble rot,' which can affect grapes, resulting in highly concentrated and sweet dessert wines.",
+  },
+  {
+    word: "Bouquet",
+    definition:
+      "The complex array of aromas in a mature wine, resulting from factors like grape variety, terroir, and winemaking techniques.",
+  },
+  {
+    word: "Brix",
+    definition:
+      "A measurement of the sugar content in grape juice or must, used to estimate the potential alcohol content of the finished wine.",
+  },
+  {
+    word: "Clone",
+    definition:
+      "A genetically identical plant or group of grapevines, derived from a single parent vine through asexual reproduction, often selected for specific desirable traits.",
+  },
+  {
+    word: "Cold soak",
+    definition:
+      "The process of chilling crushed grapes before fermentation, allowing for extended skin contact and extraction of color, flavor, and tannins without the influence of alcohol.",
+  },
+  {
+    word: "Corked",
+    definition:
+      "A term used to describe a wine that has been tainted by a cork infected with TCA (2,4,6-trichloroanisole), resulting in a musty, damp cardboard aroma and flavor.",
+  },
+  {
+    word: "Cru",
+    definition:
+      "A French term meaning 'growth,' often used to classify vineyards or wines based on quality, with classifications such as 'Grand Cru' and 'Premier Cru'.",
+  },
+  {
+    word: "Decanting",
+    definition:
+      "The process of slowly pouring wine from its bottle into a decanter to separate sediment and aerate the wine, improving its taste and aroma.",
+  },
+  {
+    word: "Diurnal range",
+    definition:
+      "The difference between daytime and nighttime temperatures in a wine-growing region, which can influence grape ripening, acidity, and flavor development.",
+  },
+  {
+    word: "Dry",
+    definition:
+      "A term used to describe a wine that has little to no residual sugar, resulting in minimal sweetness on the palate.",
+  },
+  {
+    word: "Elevage",
+    definition:
+      "A French term referring to the process of nurturing and maturing a wine after fermentation, including aging, racking, and fining, until it is ready for bottling.",
+  },
+  {
+    word: "Enology",
+    definition:
+      "The science and study of winemaking, including grape cultivation, fermentation, aging, and wine production techniques.",
+  },
+  {
+    word: "Fermentation",
+    definition:
+      "The chemical process by which yeast converts sugars in grape juice into alcohol and carbon dioxide, transforming it into wine.",
+  },
+  {
+    word: "Finish",
+    definition:
+      "The lingering aftertaste or sensation experienced after swallowing a sip of wine, used to evaluate a wine's quality and balance.",
+  },
+  {
+    word: "Fortified wine",
+    definition:
+      "A wine that has had additional alcohol, usually in the form of a neutral grape spirit, added during or after fermentation, resulting in a higher alcohol content and unique flavors.",
+  },
+  {
+    word: "Lees",
+    definition:
+      "The sediment, consisting of dead yeast cells and other particles, that settles at the bottom of a fermentation vessel, sometimes used for aging to enhance a wine's flavor and texture.",
+  },
+  {
+    "word": "Legs",
+    "definition": "The streaks of liquid that form on the inside of a wine glass after swirling, often used to assess a wine's body and alcohol content."
+},
+{
+    "word": "Malolactic fermentation",
+    "definition": "A secondary fermentation process during which bacteria convert malic acid into softer lactic acid, enhancing a wine's texture and mouthfeel."
+},
+{
+    "word": "Minerality",
+    "definition": "A term used to describe the perceived mineral-like flavors and aromas in a wine, often associated with the terroir and soil composition."
+},
+{
+    "word": "Nose",
+    "definition": "The overall aroma of a wine, encompassing its bouquet, fruit, and other scents derived from grape variety, terroir, and winemaking techniques."
+},
+{
+    "word": "Oenophile",
+    "definition": "A person who appreciates and enjoys wine, often possessing extensive knowledge about wine regions, grape varieties, and winemaking techniques."
+},
+{
+    "word": "Oak aging",
+    "definition": "The process of maturing wine in oak barrels, which imparts flavors, aromas, and tannins to the wine, enhancing its complexity and structure."
+},
+{
+    "word": "Phylloxera",
+    "definition": "A tiny, aphid-like insect that feeds on the roots of grapevines, causing significant damage and destruction to vineyards worldwide."
+},
+{
+    "word": "Racking",
+    "definition": "The process of transferring wine from one container to another, leaving sediment behind, to clarify the wine and promote its development."
+},
+{
+    "word": "Reserve",
+    "definition": "A term often found on wine labels, indicating that the wine is of higher quality, made from selected grapes or aged longer than other wines from the same winery."
+},
+{
+    "word": "Sommelier",
+    "definition": "A wine professional trained in wine service, wine and food pairing, and wine selection, often working in high-end restaurants."
+},
+{
+    "word": "Stemware",
+    "definition": "Wine glasses designed with a stem to be held without affecting the wine's temperature, with specific shapes intended to enhance the enjoyment of different wine styles
+},
+{
+    "word": "Structure",
+    "definition": "The overall impression of a wine's components, such as acidity, tannins, alcohol, and fruit, and how they interact to create a balanced and harmonious taste."
+    },
+    {
+    "word": "Sulfites",
+    "definition": "Chemical compounds, naturally occurring or added, that act as preservatives to prevent oxidation and microbial spoilage in wine."
+    },
+    {
+    "word": "Tannins",
+    "definition": "Naturally occurring compounds found in grape skins, seeds, and stems, which contribute to a wine's structure, texture, and ageability."
+    },
+    {
+    "word": "Tasting notes",
+    "definition": "Descriptive terms and observations used by wine tasters to evaluate and communicate a wine's appearance, aroma, flavor, and overall quality."
+    },
+    {
+    "word": "Terroir",
+    "definition": "The unique combination of environmental factors, including soil, climate, and topography, that give a wine its distinct characteristics."
+    },
+    {
+    "word": "Typicity",
+    "definition": "The degree to which a wine reflects its grape variety, terroir, and regional characteristics, considered an indicator of quality and authenticity."
+    },
+    {
+    "word": "Ullage",
+    "definition": "The empty space or air gap between the wine and the top of its container, such as a bottle or barrel, which can affect the wine's oxidation and aging process."
+    },
+    {
+    "word": "Veraison",
+    "definition": "The stage of grape development when the berries begin to soften, change color, and accumulate sugar, signaling the onset of ripening."
+    },
+    {
+    "word": "Vinification",
+    "definition": "The process of converting grapes into wine, including harvesting, crushing, fermenting, aging, and bottling."
+    },
+    {
+    "word": "Vinous",
+    "definition": "A term used to describe a wine that is simple and straightforward, lacking in complexity or distinction."
+    },
+    {
+    "word": "Vintage",
+    "definition": "The year in which grapes were harvested, often displayed on a wine label, indicating the wine's age and quality."
+    },
+    {
+    "word": "Viticulture",
+    "definition": "The cultivation, science, and study of grapes, specifically for wine production."
+    },
+    {
+    "word": "Yield",
+    "definition": "The quantity of grapes or wine produced per unit of land, often measured in tons per acre or hectoliters per hectare."
+    }
+    ]
+
+];
