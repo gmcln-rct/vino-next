7c88df3ef5263490a1e1c19670bb0913e2802f8f
gmcln-rct <producersama@gmail.com>
2023-04-20 18:36:34 -0400

    Work on description data

---
+++ data/country-description.js
@@ -0,0 +1,46 @@
+export COUNTRY_DESCRIPTION_DATAE = [
+    {
+      country: "Italy",
+      description: "Wine production in Italy dates back to the 8th century BCE, with the Etruscans and Greeks establishing vineyards. The Romans further developed viticulture and oenology, creating appellations and classifications. Italy is known for its diverse wine styles and grape varieties, such as Sangiovese, Nebbiolo, and Pinot Grigio."
+    },
+    {
+      country: "France",
+      description: "Wine has been produced in France since the 6th century BCE, when the Greeks founded Massalia (modern-day Marseille). The Romans expanded wine production across the country, and during the Middle Ages, monasteries played a crucial role in developing the industry. France is famous for its prestigious wine regions, including Bordeaux, Burgundy, and Champagne."
+    },
+    {
+      country: "Spain",
+      description: "Wine production in Spain began around 1100 BCE with the Phoenicians. The Romans later expanded it, and during the Moorish occupation, viticulture continued to thrive. Spain is known for its diverse wine regions, such as Rioja, Ribera del Duero, and Penedès, producing famous varieties like Tempranillo and Garnacha."
+    },
+    {
+      country: "United States",
+      description: "Wine production in the United States started in the 16th century with Spanish missionaries in present-day California. The industry expanded in the 18th and 19th centuries, but Prohibition (1920-1933) caused a major setback. The U.S. is now the fourth-largest wine producer, with California leading the way, producing varieties like Cabernet Sauvignon, Chardonnay, and Zinfandel."
+    },
+    {
+      country: "Argentina",
+      description: "Wine production in Argentina began in the 16th century with the arrival of Spanish conquistadors. The industry grew rapidly in the 19th century with European immigrants, particularly Italians. Argentina is the fifth-largest wine producer and is known for its signature grape variety, Malbec, primarily grown in the Mendoza region."
+    },
+    {
+      country: "Australia",
+      description: "Wine production in Australia started in the early 19th century, with the first vineyards planted by British settlers. The industry has grown rapidly since the 1950s, with Australia now ranking sixth in global production. Known for its innovative winemaking techniques, Australia produces popular varieties such as Shiraz, Chardonnay, and Cabernet Sauvignon."
+    },
+    {
+      country: "China",
+      description: "Wine production in China has a history dating back to the Han Dynasty (206 BCE-220 CE). The modern Chinese wine industry began in the late 19th and early 20th centuries, with a focus on domestic grape varieties like Cabernet Gernischt. Today, China is the seventh-largest wine producer, with key regions including Ningxia, Shandong, and Xinjiang."
+    },
+    {
+      country: "South Africa",
+      description: "Wine production in South Africa began in the mid-17th century, when Dutch settlers and French Huguenots planted vineyards. The industry has faced challenges, including phylloxera outbreaks and apartheid-era isolation, but has emerged as a significant player in the global market. South Africa is known for its Chenin Blanc, Pinotage, and Bordeaux-style blends."
+    },
+    {
+        country: "Germany",
+        description: "Wine production in Germany dates back to Roman times, around the 1st century CE. The industry has a strong focus on white wine production, with Riesling as the most prominent grape variety. Germany's wine regions, such as Mosel, Rheingau, and Pfalz, are known for producing high-quality wines with a wide range of sweetness levels."
+      },
+      {
+        country: "New Zealand",
+        description: "Wine production in New Zealand began in the mid-19th century with British and French settlers. The modern industry took shape in the 1970s and has since gained international acclaim. Known for its cool-climate viticulture, New Zealand produces exceptional Sauvignon Blanc, Pinot Noir, and Chardonnay, with Marlborough, Central Otago, and Hawke's Bay as key wine regions."
+      },
+      {
+        country: "Portugal",
+        description: "Wine production in Portugal started around 2000 BCE, influenced by Phoenicians, Romans, and Visigoths. The industry gained global recognition in the 18th century with the establishment of the Douro region as the world's first protected appellation. Portugal is renowned for its fortified wines, like Port and Madeira, and unique grape varieties, such as Touriga Nacional and Alvarinho."
+      }
+    ];
--- data/wine-history-timeline.data.js
+++ data/wine-history-timeline.data.js
@@ -8,26 +8,66 @@ export const WINE_HISTORY_TIMELINE_DATA = [
     event:
       "The earliest evidence of a grape and rice mixed based fermented drink sometimes compared to wine was found in ancient China.",
   },
-  { year: "5000 BCE", event:  "Evidence of wine production in Mesopotamia." },
+  { year: "5000 BCE", event: "Evidence of wine production in Mesopotamia." },
   {
     year: "3000 BCE",
     event:
       "Wine played a key role in ancient Egyptian ceremonial life. A thriving royal winemaking industry was developed in the Nile Delta following the introduction of grape cultivation from the Levant to Egypt.",
   },
-  { year: "6000 BCE", event: "Evidence of winemaking in what is now Georgia, making it one of the oldest known wine-producing regions in the world" },
-  { year: "3000 BCE", event: "Egyptian hieroglyphics depict winemaking and consumption, with wine used for religious and medicinal purposes" },
-  { year: "2000 BCE", event: "Phoenician traders introduce winemaking to the Mediterranean region, including modern-day Lebanon, Israel, and Cyprus" },
-  { year: "1500 BCE", event: "Wine is traded throughout the ancient world, with the Minoans of Crete among the most prolific wine producers and traders" },
-  { year: "1000 BCE", event: "Wine becomes an important part of Jewish religious and cultural life, with vineyards and wineries established in ancient Israel" },
+  {
+    year: "6000 BCE",
+    event:
+      "Evidence of winemaking in what is now Georgia, making it one of the oldest known wine-producing regions in the world",
+  },
+  {
+    year: "3000 BCE",
+    event:
+      "Egyptian hieroglyphics depict winemaking and consumption, with wine used for religious and medicinal purposes",
+  },
+  {
+    year: "2000 BCE",
+    event:
+      "Phoenician traders introduce winemaking to the Mediterranean region, including modern-day Lebanon, Israel, and Cyprus",
+  },
+  {
+    year: "1500 BCE",
+    event:
+      "Wine is traded throughout the ancient world, with the Minoans of Crete among the most prolific wine producers and traders",
+  },
+  {
+    year: "1000 BCE",
+    event:
+      "Wine becomes an important part of Jewish religious and cultural life, with vineyards and wineries established in ancient Israel",
+  },
   {
     year: "1000 BCE",
     event: "Wine is introduced to the Italian peninsula by the Etruscans.",
   },
-  { year: "800 BCE", event: "Greek colonists bring winemaking to Southern Italy and Sicily, leading to the establishment of some of Italy's most famous wine regions" },
-  { year: "600 BCE", event: "The Book of Genesis in the Bible first mentions the production of wine by Noah following the Great Flood." },
-  { year: "500 BCE", event: "The Romans develop vineyards throughout their empire,as an integral part of the Roman diet, Winemaking became a lucrative business." },
-  { year: "700 CE", event:  "After the Muslim conquests of 7th and 8th centruy CE, alcholic drinks were prohibited by law, but the production of wine seems to have flourished even under Islamic rule, and many khalifas used to drink alcoholic beverages during their social and private meetings." },
-  { year: "1152 CE", event:  "Bordeaux's wine owes much of its history and success to its relationship with England, which began in 1152 with the marriage of Henry II and Eleanor of Aquitaine. This alliance introduced the English to Bordeaux wine, particularly the sweet wine from the Sauternes sub-region. The trading ties between the regions lasted 300 years."},
+  {
+    year: "800 BCE",
+    event:
+      "Greek colonists bring winemaking to Southern Italy and Sicily, leading to the establishment of some of Italy's most famous wine regions",
+  },
+  {
+    year: "600 BCE",
+    event:
+      "The Book of Genesis in the Bible first mentions the production of wine by Noah following the Great Flood.",
+  },
+  {
+    year: "500 BCE",
+    event:
+      "The Romans develop vineyards throughout their empire,as an integral part of the Roman diet, Winemaking became a lucrative business.",
+  },
+  {
+    year: "700 CE",
+    event:
+      "After the Muslim conquests of 7th and 8th centruy CE, alcholic drinks were prohibited by law, but the production of wine seems to have flourished even under Islamic rule, and many khalifas used to drink alcoholic beverages during their social and private meetings.",
+  },
+  {
+    year: "1152 CE",
+    event:
+      "Bordeaux's wine owes much of its history and success to its relationship with England, which began in 1152 with the marriage of Henry II and Eleanor of Aquitaine. This alliance introduced the English to Bordeaux wine, particularly the sweet wine from the Sauternes sub-region. The trading ties between the regions lasted 300 years.",
+  },
   {
     year: "1300 CE",
     event:
@@ -38,156 +78,270 @@ export const WINE_HISTORY_TIMELINE_DATA = [
     event:
       "Wine production in the New World begins with Spanish and Portuguese colonization of the Americas.",
   },
-  { year: "1600 CE", event:  "The first wine is produced in South Africa." },
+  { year: "1600 CE", event: "The first wine is produced in South Africa." },
 
   {
     year: "1700 CE",
     event:
-    "The wine industry grows in popularity in the American colonies. The first winery in the United States is established in Virginia.",
-    },
-    {
+      "The wine industry grows in popularity in the American colonies. The first winery in the United States is established in Virginia.",
+  },
+  {
     year: "1756 CE",
     event:
-    "The first wine-growing region in the world is officially established in the Tokaj region of Hungary.",
-    },
-    {
+      "The first wine-growing region in the world is officially established in the Tokaj region of Hungary.",
+  },
+  {
     year: "1765 CE",
     event:
-    "Thomas Jefferson attempts to cultivate European grape varieties at his Virginia plantation, Monticello.",
-    },
-    {
+      "Thomas Jefferson attempts to cultivate European grape varieties at his Virginia plantation, Monticello.",
+  },
+  {
     year: "1775 CE",
     event:
-    "The first commercial vineyard in the United States is established in Pennsylvania.",
-    },
-    {
+      "The first commercial vineyard in the United States is established in Pennsylvania.",
+  },
+  {
     year: "1800 CE",
     event:
-    "The phylloxera epidemic devastates European vineyards, leading to the adoption of grafting techniques.",
-    },
-    {
+      "The phylloxera epidemic devastates European vineyards, leading to the adoption of grafting techniques.",
+  },
+  {
     year: "1863 CE",
     event: "The first wine classification system is established in Bordeaux.",
-    },
-    {
+  },
+  {
     year: "1875 CE",
     event: "The first wine cooperative is founded in France.",
-    },
-    {
+  },
+  {
     year: "1880 CE",
     event: "Wine production in Australia and New Zealand begins.",
-    },
-    {
+  },
+  {
     year: "1882 CE",
-    event: "Baron Edmond de Rothschild begins investing in wineries in Palestine (modern-day Israel).",
-    },
-    {
+    event:
+      "Baron Edmond de Rothschild begins investing in wineries in Palestine (modern-day Israel).",
+  },
+  {
     year: "1890 CE",
     event: "Significant wine production in Argentina and Chile begins.",
-    },
-    {
+  },
+  {
     year: "1900 CE",
     event: "Wine production in South Africa reaches new heights.",
-    },
-    {
+  },
+  {
     year: "1919 CE",
     event:
-    "Prohibition begins in the United States, leading to the decline of the American wine industry.",
-    },
-    {
+      "Prohibition begins in the United States, leading to the decline of the American wine industry.",
+  },
+  {
     year: "1940 CE",
     event: "The wine industry in Europe is further impacted by World War II.",
-    },
-    {
+  },
+  {
     year: "1945 CE",
     event: "Excellent vintage in Bordeaux, particularly for the Right Bank.",
-    },
-    {
+  },
+  {
     year: "1947 CE",
     event: "Great vintage in Bordeaux, particularly for the Left Bank.",
-    },
-    {
+  },
+  {
     year: "1952 CE",
     event:
-    "First vintage of Chateau Margaux produced after the vineyard was replanted following World War II.",
-    },
-    {
+      "First vintage of Chateau Margaux produced after the vineyard was replanted following World War II.",
+  },
+  {
     year: "1953 CE",
     event:
-    "German wine industry begins to recover from the devastation of World War II.",
-    },
-    {
+      "German wine industry begins to recover from the devastation of World War II.",
+  },
+  {
     year: "1955 CE",
-    event:
-    "Robert Mondavi opens his own winery in Napa Valley, California.",
-    },
-    {
+    event: "Robert Mondavi opens his own winery in Napa Valley, California.",
+  },
+  {
     year: "1957 CE",
     event:
-    "Development of mechanical grape harvesters revolutionizes wine production.",
-    },
-    {
+      "Development of mechanical grape harvesters revolutionizes wine production.",
+  },
+  {
     year: "1961 CE",
-    event: "Legendary vintage in Bordeaux, considered one of the greatest of the century, particularly for the Left Bank.",
-    },
-    {
+    event:
+      "Legendary vintage in Bordeaux, considered one of the greatest of the century, particularly for the Left Bank.",
+  },
+  {
     year: "1965 CE",
     event:
-    "Introduction of temperature-controlled fermentation techniques improves wine quality.",
-    },
-    {
+      "Introduction of temperature-controlled fermentation techniques improves wine quality.",
+  },
+  {
     year: "1968 CE",
     event:
-    "First wine competition held in the United States, the Los Angeles County Fair Commercial Wine Competition.",
-    },
-    {
+      "First wine competition held in the United States, the Los Angeles County Fair Commercial Wine Competition.",
+  },
+  {
     year: "1970 CE",
     event:
-    "Rise of the wine critic, with publications such as Wine Spectator and Wine Advocate becoming influential.",
-    },
-    {
+      "Rise of the wine critic, with publications such as Wine Spectator and Wine Advocate becoming influential.",
+  },
+  {
     year: "1971 CE",
+    event: "New Zealand begins to emerge as a major wine-producing country.",
+  },
+
+  {
+    year: "1971 CE",
+    event: "New Zealand begins to emerge as a major wine-producing country",
+  },
+  {
+    year: "1972 CE",
     event:
-    "New Zealand begins to emerge as a major wine-producing country.",
-    },
+      "Publication of the first comprehensive guide to wine, The World Atlas of Wine by Hugh Johnson and Jancis Robinson",
+  },
+  {
+    year: "1973 CE",
+    event:
+      "First vintage of Opus One, a collaboration between Robert Mondavi and Baron Philippe de Rothschild",
+  },
+  {
+    year: "1974 CE",
+    event:
+      "Introduction of the Wine of Origin system in South Africa to regulate wine production",
+  },
+  {
+    year: "1975 CE",
+    event: "Development of the first wine bottle with a screw cap",
+  },
+  {
+    year: "1976 CE",
+    event:
+      "The 'Judgment of Paris' blind tasting event takes place, with California wines beating French wines",
+  },
+  {
+    year: "1980 CE",
+    event:
+      "Establishment of the Australian Wine Research Institute to improve wine quality and production",
+  },
+  {
+    year: "1983 CE",
+    event:
+      "First vintage of Caymus Special Selection, which becomes one of California's most iconic wines",
+  },
+  {
+    year: "1985 CE",
+    event:
+      "Introduction of the first wine label health warning in the United States",
+  },
+  {
+    year: "1990 CE",
+    event:
+      "Phylloxera, a vine disease, is discovered in Napa Valley, leading to the replanting of many vineyards",
+  },
+  {
+    year: "1993 CE",
+    event:
+      "Production of ice wine becomes more popular in Canada and other cold-climate regions",
+  },
+  {
+    year: "1959 CE",
+    event:
+      "One of the greatest vintages of the 20th century in Bordeaux, particularly for the Left Bank",
+  },
+  {
+    year: "1961 CE",
+    event:
+      "Legendary vintage in Bordeaux, considered one of the greatest of the century, particularly for the Left Bank",
+  },
+  {
+    year: "1982 CE",
+    event:
+      "Legendary vintage in Bordeaux, considered one of the greatest of the century, particularly for the Left Bank",
+  },
 
-  { year: "1971 CE", event:  "New Zealand begins to emerge as a major wine-producing country" },
-  { year: "1972 CE", event:  "Publication of the first comprehensive guide to wine, The World Atlas of Wine by Hugh Johnson and Jancis Robinson" },
-  { year: "1973 CE", event:  "First vintage of Opus One, a collaboration between Robert Mondavi and Baron Philippe de Rothschild" },
-  { year: "1974 CE", event:  "Introduction of the Wine of Origin system in South Africa to regulate wine production" },
-  { year: "1975 CE", event:  "Development of the first wine bottle with a screw cap" },
-  { year: "1976 CE", event:  "The 'Judgment of Paris' blind tasting event takes place, with California wines beating French wines" },
-  { year: "1980 CE", event:  "Establishment of the Australian Wine Research Institute to improve wine quality and production" },
-  { year: "1983 CE", event:  "First vintage of Caymus Special Selection, which becomes one of California's most iconic wines" },
-  { year: "1985 CE", event:  "Introduction of the first wine label health warning in the United States" },
-  { year: "1990 CE", event:  "Phylloxera, a vine disease, is discovered in Napa Valley, leading to the replanting of many vineyards" },
-  { year: "1993 CE", event:  "Production of ice wine becomes more popular in Canada and other cold-climate regions" },
-  { year: "1959 CE", event:  "One of the greatest vintages of the 20th century in Bordeaux, particularly for the Left Bank" },
-  { year: "1961 CE", event:  "Legendary vintage in Bordeaux, considered one of the greatest of the century, particularly for the Left Bank" },
-  { year: "1982 CE", event:  "Legendary vintage in Bordeaux, considered one of the greatest of the century, particularly for the Left Bank" },
-
-  { year: "1995 CE", event:  "Adoption of new wine labeling laws in the United States that require listing of alcohol content and nutritional information" },
+  {
+    year: "1995 CE",
+    event:
+      "Adoption of new wine labeling laws in the United States that require listing of alcohol content and nutritional information",
+  },
   {
     year: "1997 CE",
     event:
       "The Wine Institute of California establishes the Code of Sustainable Winegrowing Practices.",
   },
-  { year: "2001 CE", event:  "Global warming concerns raised about the future of wine production" },
-  { year: "2003 CE", event:  "Robert Parker gives a perfect 100-point score to a California wine for the first time" },
-  { year: "2004 CE", event:  "Methanol contamination in wine kills more than 20 people in Europe" },
-  { year: "2006 CE", event:  "Biodynamic wine production gains popularity" },
-  { year: "2008 CE", event:  "China becomes a major wine producer and consumer" },
-  { year: "2010 CE", event:  "Legendary vintage in Bordeaux, particularly for the Left Bank" },
-  
-  { year: "2010 CE", event:  "Natural wine movement gains momentum, with producers focusing on minimal intervention and organic farming" },
-  { year: "2012 CE", event:  "Canned wine gains popularity as a convenient and portable option" },
-  { year: "2014 CE", event:  "Wine tourism becomes a growing industry, with wineries and regions offering immersive experiences" },
-  { year: "2015 CE", event:  "New York Times exposé reveals labor abuses in California vineyards" },
-  { year: "2015 CE", event:  "Great vintage in Bordeaux, particularly for the Right Bank" },
-  { year: "2016 CE", event:  "Excellent vintage in Bordeaux, particularly for the Left Bank" },
-  { year: "2017 CE", event:  "Wildfires devastate California wine country, leading to a decrease in production and quality" },
-  { year: "2019 CE", event:  "Climate change accelerates, with some regions experiencing droughts and extreme weather events that impact wine production" },
-  { year: "2020 CE", event:  "Challenging vintage due to weather conditions, but some promising wines on both the Left and Right Bank" },
-  { year: "2021 CE", event:  "Frost and heatwaves lead to a smaller harvest, but some exceptional wines on both the Left and Right Bank" },
-  { year: "2021 CE", event:  "Online wine sales skyrocket during the COVID-19 pandemic as consumers shift to e-commerce" }
+  {
+    year: "2001 CE",
+    event: "Global warming concerns raised about the future of wine production",
+  },
+  {
+    year: "2003 CE",
+    event:
+      "Robert Parker gives a perfect 100-point score to a California wine for the first time",
+  },
+  {
+    year: "2004 CE",
+    event: "Methanol contamination in wine kills more than 20 people in Europe",
+  },
+  { year: "2006 CE", event: "Biodynamic wine production gains popularity" },
+  {
+    year: "2008 CE",
+    event: "China becomes a major wine producer and consumer",
+  },
+  {
+    year: "2010 CE",
+    event: "Legendary vintage in Bordeaux, particularly for the Left Bank",
+  },
+
+  {
+    year: "2010 CE",
+    event:
+      "Natural wine movement gains momentum, with producers focusing on minimal intervention and organic farming",
+  },
+  {
+    year: "2012 CE",
+    event: "Canned wine gains popularity as a convenient and portable option",
+  },
+  {
+    year: "2014 CE",
+    event:
+      "Wine tourism becomes a growing industry, with wineries and regions offering immersive experiences",
+  },
+  {
+    year: "2015 CE",
+    event: "New York Times exposé reveals labor abuses in California vineyards",
+  },
+  {
+    year: "2015 CE",
+    event: "Great vintage in Bordeaux, particularly for the Right Bank",
+  },
+  {
+    year: "2016 CE",
+    event: "Excellent vintage in Bordeaux, particularly for the Left Bank",
+  },
+  {
+    year: "2017 CE",
+    event:
+      "Wildfires devastate California wine country, leading to a decrease in production and quality",
+  },
+  {
+    year: "2019 CE",
+    event:
+      "Climate change accelerates, with some regions experiencing droughts and extreme weather events that impact wine production",
+  },
+  {
+    year: "2020 CE",
+    event:
+      "Challenging vintage due to weather conditions, but some promising wines on both the Left and Right Bank",
+  },
+  {
+    year: "2021 CE",
+    event:
+      "Frost and heatwaves lead to a smaller harvest, but some exceptional wines on both the Left and Right Bank",
+  },
+  {
+    year: "2021 CE",
+    event:
+      "Online wine sales skyrocket during the COVID-19 pandemic as consumers shift to e-commerce",
+  },
 ];
