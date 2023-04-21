8fb7813f81aaa7e677ac9bad25866c56584413e7
gmcln-rct <producersama@gmail.com>
2023-04-21 11:41:30 -0400

    Update grape index masthead

--- pages/grapes/index.js
+++ pages/grapes/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { GRAPES_DATA } from "@/data/grape-data";
 import { getTopData } from "@/data/utils";
 import DataList from "@/components/layout/data-list";
+import Masthead from "@/components/layout/masthead";
 
 export default function GrapeIndex({ topGrapes }) {
   return (
@@ -13,8 +14,13 @@ export default function GrapeIndex({ topGrapes }) {
           content="Data visualization of grape wine production for all wine-producing countries"
         />
       </Head>
-      <h1 className="indexheader">Grapes Index</h1>
+      <Masthead
+        backgroundImage="/images/site-images/index-masthead-grape.jpg"
+        header="Wine Grapes"
+        headerText="Learn about individual grape varietals through our customizable data charts or sections for individual grapes."
+      />
       {/* <h2>Featured Countries</h2> */}
+
       <DataList items={topGrapes} />
     </div>
   );
