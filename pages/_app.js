7e1196af0594fb339745eae18e465e86939b445e
gmcln-rct <producersama@gmail.com>
2023-04-25 14:39:14 -0400

    Add gtm

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 
 import Layout from "../components/layout/layout";
 // import HomePage from './index';
@@ -8,19 +9,46 @@ import "../styles/global.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Layout >
+      <Layout>
         <Head>
           <title>Winography - Wine Data Visualizations</title>
           <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta name='robots' content='index, follow'></meta>
-          <meta name="keywords" content="wine, data visualization, wine education, learning" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta name="robots" content="index, follow"></meta>
+          <meta
+            name="keywords"
+            content="wine, data visualization, wine education, learning"
+          />
           <meta name="author" content="Glenn McClanan, Rio Create LLC" />
-          <meta property="og:title" content="Winography - Wine Data Visualizations" />
-          <meta property="og:description" content="Wine data visualizations for wine education and learning" />
-          <meta property="og:image" content="https://winography.net/images/site-images/homepage-masthead.jpg" />
+          <meta
+            property="og:title"
+            content="Winography - Wine Data Visualizations"
+          />
+          <meta
+            property="og:description"
+            content="Wine data visualizations for wine education and learning"
+          />
+          <meta
+            property="og:image"
+            content="https://winography.net/images/site-images/homepage-masthead.jpg"
+          />
           <meta property="og:url" content="https://winography.net" />
-          
+
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {/* Google tag (gtag.js) */}
+            <script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=G-X1PYJEHQHJ"
+            ></script>
+            <script>
+              window.dataLayer = window.dataLayer || []; function gtag()
+              {dataLayer.push(arguments)}
+              gtag('js', new Date()); gtag('config', 'G-X1PYJEHQHJ');
+            </script>
+          </Script>
         </Head>
         <Component {...pageProps} />
       </Layout>
