e84c076c8627212e192c6831b7b258627767b79b
gmcln-rct <producersama@gmail.com>
2023-05-03 16:20:57 -0400

    Update header icon color

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -3,7 +3,16 @@ import Link from "next/link";
 // import Image from "next/image";
 // import { CSSTransition } from "react-transition-group";
 import classes from "./main-header.module.css";
-import { AppBar, Toolbar, IconButton, Typography, Drawer, List, ListItem, ListItemText } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
@@ -28,64 +37,94 @@ const Navbar = () => {
       <AppBar position="static" color="inherit">
         <Toolbar className={classes.navContainer}>
           <div className={classes.brand}>
-            <Link href="/">Winography <span className={classes.sitebeta}>Beta</span></Link>
+            <Link href="/">
+              Winography <span className={classes.sitebeta}>Beta</span>
+            </Link>
           </div>
           <div>
-          <nav className={classes.listContainer}>
-            <ul className={classes.list}>
-              <li className={classes.listitem} onClick={toggleDrawer}>
-                Data Visualizations
-              </li>
-              <li className={classes.listitem}>
-                <Link href="/resources">Resources</Link>
-              </li>
-              <li className={classes.listitem}>
-                <Link href="/quiz/">Quiz</Link>
-              </li>
-              <li className={classes.listitem}>
-                <Link href="/about">About</Link>
-              </li>
-            </ul>
-          </nav>
-          <IconButton
-            edge="end"
-            color="#a10101"
-            aria-label="menu"
-            display={{ xs: "block", sm: "none" }}
-            className={classes.menuButton}
-            onClick={toggleDrawer}
-          >
-            <MenuIcon />
-          </IconButton>
+            <nav className={classes.listContainer}>
+              <ul className={classes.list}>
+                <li className={classes.listitem} onClick={toggleDrawer}>
+                  Data Visualizations
+                </li>
+                <li className={classes.listitem}>
+                  <Link href="/resources">Resources</Link>
+                </li>
+                <li className={classes.listitem}>
+                  <Link href="/quiz/">Quiz</Link>
+                </li>
+                <li className={classes.listitem}>
+                  <Link href="/about">About</Link>
+                </li>
+              </ul>
+            </nav>
+            <IconButton
+              edge="end"
+              aria-label="menu"
+              display={{ xs: "block", sm: "none" }}
+              className={classes.menuButton}
+              onClick={toggleDrawer}
+            >
+              <MenuIcon sx={{ color: "a10101" }} />
+            </IconButton>
           </div>
         </Toolbar>
       </AppBar>
-      <Drawer
-        anchor="right"
-        open={drawerOpen}
-        onClose={toggleDrawer}
-      >
+      <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer}>
         <List className={classes.drawer}>
           <p className={classes.listHeader}>Data Visualizations:</p>
           <div className={classes.listDataViz}>
-          <ListItem button onClick={toggleDrawer} component={Link} href="/countries">
-            <ListItemText primary="Countries" />
-          </ListItem>
-          <ListItem button onClick={toggleDrawer} component={Link} href="/grapes">
-            <ListItemText primary="Grapes" />
-          </ListItem>
-          <ListItem button onClick={toggleDrawer} component={Link} href="/historic">
-            <ListItemText primary="Historic Data" />
-          </ListItem>
+            <ListItem
+              button
+              onClick={toggleDrawer}
+              component={Link}
+              href="/countries"
+            >
+              <ListItemText primary="Countries" />
+            </ListItem>
+            <ListItem
+              button
+              onClick={toggleDrawer}
+              component={Link}
+              href="/grapes"
+            >
+              <ListItemText primary="Grapes" />
+            </ListItem>
+            <ListItem
+              button
+              onClick={toggleDrawer}
+              component={Link}
+              href="/historic"
+            >
+              <ListItemText primary="Historic Data" />
+            </ListItem>
           </div>
           <div className={classes.horizontalLine}></div>
-          <ListItem button onClick={toggleDrawer} className={classes.mobileOnly} component={Link} href="/resources">
+          <ListItem
+            button
+            onClick={toggleDrawer}
+            className={classes.mobileOnly}
+            component={Link}
+            href="/resources"
+          >
             <ListItemText primary="Resources" />
           </ListItem>
-          <ListItem button onClick={toggleDrawer} className={classes.mobileOnly} component={Link} href="/quiz">
+          <ListItem
+            button
+            onClick={toggleDrawer}
+            className={classes.mobileOnly}
+            component={Link}
+            href="/quiz"
+          >
             <ListItemText primary="Quiz" />
           </ListItem>
-          <ListItem button onClick={toggleDrawer} className={classes.mobileOnly} component={Link} href="/about">
+          <ListItem
+            button
+            onClick={toggleDrawer}
+            className={classes.mobileOnly}
+            component={Link}
+            href="/about"
+          >
             <ListItemText primary="About" />
           </ListItem>
         </List>
@@ -95,4 +134,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
