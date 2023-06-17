8fffe0b55bb23c7e0029321aa96db82975fefb73
gmcln-rct <producersama@gmail.com>
2023-06-17 14:43:38 -0400

    Work on chart selector

--- components/charts/chart-selector-dual.js
+++ components/charts/chart-selector-dual.js
@@ -1,18 +1,29 @@
 import classes from "./chart-selector.module.css";
 
 const ChartSelectorDual = ({ selectedGrapeType, setSelectedGrapeType }) => {
-    return (
-      <>
-        <select
-          className={classes.selectCss}
-          value={selectedGrapeType}
-          onChange={(event) => setSelectedGrapeType(event.target.value)}
-        >
-          <option value="Red">Red Grapes</option>
-          <option value="White">White Grapes</option>
-        </select>
-      </>
-    );
-  };
-  
-  export default ChartSelectorDual;
+  return (
+    <>
+      <select
+        value={selectedCountry}
+        className={classes.selectCss}
+        onChange={handleCountryChange}
+      >
+        {COUNTRIES.map((country) => (
+          <option key={country.id} value={country.id}>
+            {country.itemName}
+          </option>
+        ))}
+      </select>
+      <select
+        className={classes.selectCss}
+        value={selectedGrapeType}
+        onChange={(event) => setSelectedGrapeType(event.target.value)}
+      >
+        <option value="Red">Red Grapes</option>
+        <option value="White">White Grapes</option>
+      </select>
+    </>
+  );
+};
+
+export default ChartSelectorDual;
