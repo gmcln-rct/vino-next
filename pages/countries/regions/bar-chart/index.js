6bfa476014166b03567dc885f7965635eea9e15b
gmcln-rct <producersama@gmail.com>
2023-08-06 19:48:40 -0400

    Update with topo json

--- pages/countries/regions/bar-chart/index.js
+++ pages/countries/regions/bar-chart/index.js
@@ -91,7 +91,7 @@ function RegionsBarChartPage() {
     <>
       <Head>
         <title>
-          Wine Production By Region - Bar Chart - Winography | Learn About Wine Through Data Visualizations
+          Wine Grape Production By Region - Bar Chart - Winography | Learn About Wine Through Data Visualizations
         </title>
         <meta
           name="description"
@@ -99,7 +99,7 @@ function RegionsBarChartPage() {
         />
       </Head>
       <section className={classes.chart}>
-        <h1 className={classes.header}>Wine Production By Region: {country.itemName}</h1>
+        <h1 className={classes.header}>Grape Production By Region: {country.itemName}</h1>
         <h2 className={classes.subheader}>
           Production of world top 100 wine grapes in individual regions.
         </h2>
