df43f7d9ffbd965171a641f48cad0c5627616c1f
Glenn McClanan <34251951+gmcln-rct@users.noreply.github.com>
2025-08-29 14:16:33 -0400

    Work on build error 5

--- next.config.js
+++ next.config.js
@@ -5,18 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  },
 }
 
 module.exports = nextConfig
